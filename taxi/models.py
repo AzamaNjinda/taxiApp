67696eaf28b52fe55fbcac356e6ad0544c24d1a9
asong mac-nelly <asongnelly58@gmail.com>
2023-08-23 14:26:01 +0100

    8

--- taxi/models.py
+++ taxi/models.py
@@ -1,3 +1,10 @@
 from django.db import models
 
 # Create your models here.
+class Credit(models.Model):
+ Amount = models.FloatField()
+ phone_number = models.CharField(max_length=100)
+ credit = models.FloatField()
+ date = models.DateTimeField(auto_now_add=True)
+#addthumnial later
+#add in author later
