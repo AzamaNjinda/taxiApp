87dc7ed51137ae70871f550eae8dca62fd5fc5da
Changoh Collins <changohcollins@gmail.com>
2023-09-28 17:48:27 -0700

    feedback model added

--- taxi/admin.py
+++ taxi/admin.py
@@ -1,4 +1,6 @@
 from django.contrib import admin
 from .models import Credit
+from .models import feedback
 # Register your models here.
 admin.site.register(Credit)
+admin.site.register(feedback)
--- taxi/models.py
+++ taxi/models.py
@@ -6,4 +6,10 @@ class Credit(models.Model):
     phone_number = models.CharField(max_length=15)
     credit = models.FloatField()
     date = models.DateTimeField(auto_now_add=True)
+
+class feedback(models.Model):
+    username = models.CharField(max_length=30)
+    feedback = models.TextField()
+    subject = models.CharField(max_length=100)
+    date = models.DateTimeField(auto_now_add=True)
     
